706f9c70a6d531c5176cbdb98dffe3221674e81f
serkoba <ser_a_koba@hotmail.com>
2017-03-28 11:55:14 -0300

    v13

--- www/js/index.js
+++ www/js/index.js
@@ -16,72 +16,72 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    /*},
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+//var app = {
+//    // Application Constructor
+//    initialize: function() {
+//        this.bindEvents();
+//    },
+//    // Bind Event Listeners
+//    //
+//    // Bind any events that are required on startup. Common events are:
+//    // 'load', 'deviceready', 'offline', and 'online'.
+//    bindEvents: function() {
+//        document.addEventListener('deviceready', this.onDeviceReady, false);
+//    },
+//    // deviceready Event Handler
+//    //
+//    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+//    // function, we must explicitly call 'app.receivedEvent(...);'
+//    onDeviceReady: function() {
+//        app.receivedEvent('deviceready');
+//    /*},
+//    // Update DOM on a Received Event
+//    receivedEvent: function(id) {
+//        var parentElement = document.getElementById(id);
+//        var listeningElement = parentElement.querySelector('.listening');
+//        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-        console.log('Received Event: ' + id);
+//        listeningElement.setAttribute('style', 'display:none;');
+//        receivedElement.setAttribute('style', 'display:block;');
+//        console.log('Received Event: ' + id);
         
-        var pushNotification = window.plugins.pushNotification;
-        pushNotification.register(app.successHandler, app.errorHandler, { "senderID": "AIzaSyCr2a_B2wL8KfYxusSOuX06QaU5QUF20n0", "ecb": "com.innovacion.TurneroClient" });
+//        var pushNotification = window.plugins.pushNotification;
+//        pushNotification.register(app.successHandler, app.errorHandler, { "senderID": "AIzaSyCr2a_B2wL8KfYxusSOuX06QaU5QUF20n0", "ecb": "com.innovacion.TurneroClient" });
        
 
-        },
-    // result contains any message sent from the plugin call
-    successHandler: function(result) {
-        alert('Callback Success! Result = '+result)
-    },
-    errorHandler: function (result)
-    {
-        alert(error);
-    },
-    onNotificationGCM: function (e) {
-        switch (e.event) {
-            case 'registered':
-                if (e.regid.length > 0) {
-                    console.log("Regid " + e.regid);
-                    alert('registration id = ' + e.regid);
-                }
-                break;
+//        },
+//    // result contains any message sent from the plugin call
+//    successHandler: function(result) {
+//        alert('Callback Success! Result = '+result)
+//    },
+//    errorHandler: function (result)
+//    {
+//        alert(error);
+//    },
+//    onNotificationGCM: function (e) {
+//        switch (e.event) {
+//            case 'registered':
+//                if (e.regid.length > 0) {
+//                    console.log("Regid " + e.regid);
+//                    alert('registration id = ' + e.regid);
+//                }
+//                break;
 
-            case 'message':
-                // this is the actual push notification. its format depends on the data model from the push server
-                alert('message = ' + e.message + ' msgcnt = ' + e.msgcnt);
-                break;
+//            case 'message':
+//                // this is the actual push notification. its format depends on the data model from the push server
+//                alert('message = ' + e.message + ' msgcnt = ' + e.msgcnt);
+//                break;
 
-            case 'error':
-                alert('GCM error = ' + e.msg);
-                break;
+//            case 'error':
+//                alert('GCM error = ' + e.msg);
+//                break;
 
-            default:
-                alert('An unknown GCM event has occurred');
-                break;
-        }*/
-    }
-};
+//            default:
+//                alert('An unknown GCM event has occurred');
+//                break;
+//        }*/
+//    }
+//};
 
 function TimerStatusChange(objText) {
     var stringUri = "http://bahiatransporte.com.ar/turnos/index.php?id=" + $("#slcSucursal").val();
